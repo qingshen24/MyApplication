5f485b7420b2ca8f289bdbb23134efeafae61d42
qingshen24 <qingshen42@sina.com>
2015-12-29 10:18:02 +0800

    modified the layout

---
+++ app/src/main/java/com/example/add/myapplication/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.add.myapplication;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.Utils;
+//import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.objdetect.CascadeClassifier;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.FaceDetector;
+import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    private Button btn;
+
+    private Bitmap srcBitmap;
+    private Bitmap grayBitmap;
+    private static boolean flag = true;
+    private static boolean isFirst = true;
+    private static final String TAG = "qing_shen";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.img);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new ProcessClickListener());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, getApplicationContext(), mLoaderCallback);
+        Log.i(TAG, "onResume sucess load OpenCV...");
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:
+                    Log.i(TAG, "加载成功");
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    Log.i(TAG, "加载失败");
+                    break;
+            }
+        }
+    };
+
+
+    public void procSrc2Gray() {
+        Mat rgbMat = new Mat();
+        Mat grayMat = new Mat();
+        srcBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.genie);
+        grayBitmap = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), Bitmap.Config.RGB_565);
+        Utils.bitmapToMat(srcBitmap, rgbMat);//convert the bitMap to Mat
+        Imgproc.cvtColor(rgbMat, grayMat, Imgproc.COLOR_RGB2GRAY);//using the function of OpneCV to convert to the RGB to gray
+        //进行人脸检测
+
+
+        Imgproc.equalizeHist(grayMat, grayMat);//直方图均衡化
+        /*
+        CascadeClassifier faceDector = new CascadeClassifier(FaceDetector.class.getResource("haarcascade_frontalface_alt.xml").getPath());
+        MatOfRect faceDetectios =new MatOfRect();
+        faceDector.detectMultiScale(grayMat, faceDetectios);
+        for (Rect rect: faceDetectios.toArray()){
+            Imgproc.rectangle(grayMat,new Point(rect.x,rect.y),new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,255,0));
+        }
+        */
+
+        //Imgproc.Canny(grayMat,grayMat,100.0,2.0);//边缘检测
+        Utils.matToBitmap(grayMat, grayBitmap);//convert Mat to bitMap
+        Log.i(TAG, "procSrc2Gray sucess...");
+    }
+
+    public class ProcessClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (isFirst) {
+                procSrc2Gray();
+                isFirst = false;
+            }
+            if (flag) {
+                img.setImageBitmap(grayBitmap);
+                btn.setText("查看原图");
+                flag = false;
+            } else {
+                img.setImageBitmap(srcBitmap);
+                btn.setText("灰度化");
+                flag = true;
+            }
+        }
+    }
+}
+
+
+
+
+
